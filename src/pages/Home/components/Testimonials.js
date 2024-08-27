159a8f7628a296a691498ddca3e6380ed65d5e87
Dacs Dacallos <dacsstack@gmail.com>
2024-08-27 09:45:22 -0800

    Change testimonial

--- src/pages/Home/components/Testimonials.js
+++ src/pages/Home/components/Testimonials.js
@@ -1,10 +1,8 @@
-import Avatar from "../../assets/6858504.png";
-
 export const Testimonials = () => {
   return (
     <section className="my-20">
       <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">
-        Lorem, ipsum dolor.
+        Testimonial
       </h1>
       <div className="grid mb-8 rounded-lg border border-gray-200 shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
         <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
@@ -18,7 +16,11 @@ export const Testimonials = () => {
             </p>
           </blockquote>
           <figcaption className="flex justify-center items-center space-x-3">
-            <img className="w-9 h-9 rounded-full" src={Avatar} alt="user" />
+            <img
+              className="w-9 h-9 rounded-full"
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=120&q=50"
+              alt="user"
+            />
             <div className="space-y-0.5 font-medium dark:text-white text-left">
               <div>Lorem, ipsum dolor.</div>
               <div className="text-sm font-light text-gray-500 dark:text-gray-400">
